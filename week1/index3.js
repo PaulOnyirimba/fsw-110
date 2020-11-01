4333cd8f7cf6158df5b32a753ece394b5057f700
PaulOnyirimba <Paul.Onyirimba@bryanuniversity.edu>
2020-11-01 16:34:45 -0500

    test

--- week1/index3.js
+++ week1/index3.js
@@ -1,1 +1,5 @@
-document.getElementById("welcome").innerHTML = "Welcome To MY Website"
+
+var newH1 = document.createElement("H1")
+newH1.textContent = "Welcome to my JS site"
+var myH1 = document.getElementById("my-h1")
+myH1.append(newH1)
