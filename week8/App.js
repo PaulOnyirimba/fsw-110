f5db05a7df977a4570739ed25f4dcc0a7d908557
PaulOnyirimba <Paul.Onyirimba@bryanuniversity.edu>
2020-12-21 23:57:03 -0500

    test

---
+++ week8/App.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+function App() {
+return (
+<div>
+<input type="checkbox" />
+<p>Get groceries</p>
+<input type="checkbox" />
+<p>Wash car</p>
+<input type="checkbox" />
+<p>Do laundry</p>
+</div>
+)
+
+
+
+}
+
+export default App
